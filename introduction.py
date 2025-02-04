dc47af80a2121b0580e2258ce6e9a5c5534884a2
cherop-26 <cherrymutai@gmail.com>
2025-02-04 14:18:35 +0300

    fourth save

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display a value
+
+print("cheryl")
+print("emobilis")
+print(100)
+print(10 + 30)
---
+++ variable.py
@@ -0,0 +1,29 @@
+# variable is a container used to store a value
+
+location ="westlands"
+firstname ="Cheryl"
+lastname ="emobilis"
+school ="emobilis"
+x=39
+y=2
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname,lastname,)
+print("my full name is cheryl")
+print(school," is a coding school")
+print(x + y)
+
+print("the product of x and y is",x*y)
+
+
+
+
+
+
+
+
+
+
