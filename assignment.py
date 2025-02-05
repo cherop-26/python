c7ea65bbba37a93b5049018767837a0f4f90ad32
cherop-26 <cherrymutai@gmail.com>
2025-02-05 18:00:23 +0300

    fourth save

--- assignment.py
+++ assignment.py
@@ -1,9 +1,15 @@
 # leap year
-year = 2024
-if year +-== 4
-    print(x,"is a leap year")
-
+year = 2025
+if ( year%4==0 and year%100!=0 ):
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
 
 #checks whether a letter is a vowel or a consonant
+vowel = l
+if (vowel in ["a","e","i","o","u"]):
+    print(vowel,"is a vowel")
+else:
+    print(vowel,"is not a consonant")
 
 
