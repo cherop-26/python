a974316b7b2ca3aecb7723c74cf3262dee8c14c0
cherop-26 <cherrymutai@gmail.com>
2025-02-10 14:12:51 +0300

    fourth save

--- primenumber.py
+++ primenumber.py
@@ -1,12 +1,28 @@
-# python program ,enter number define if it is prime
-
-number = int(input("Enter a number: "))
 
 
-for i in range(2, int(number ** 0.5) + 1):
-    print( number," is a not prime number")
-else :
-    print(number," is  a prime number")
+# Program to check if a number is prime or not
+
+
+num = int(input("Enter a number: "))
+
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
 
 
 
--- simplecalculator.py
+++ simplecalculator.py
@@ -2,10 +2,45 @@
 #enter number *2 ,Result
 #notify if the operator is invalid
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-operator = input("Enter operator: ")
-for operator in range('+','-','*','/');
+def add(x, y):
+    return x + y
 
-equals = num1
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error!"
+    else:
+        return x / y
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+def calculator():
+    print("Select operator:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+operator= input("Enter operator (1/2/3/4): ")
+
+if operator == '1':
+        print(f"{num1} + {num2} = {add(num1, num2)}")
+elif operator == '2':
+        print(f"{num1} - {num2} = {subtract(num1, num2)}")
+elif operator == '3':
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif operator == '4':
+        print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+        print("Invalid input!")
+
+
+calculator()
+
