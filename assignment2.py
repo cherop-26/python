74137baa3cf16f5261d6e440994c69e96b7a888c
cherop-26 <cherrymutai@gmail.com>
2025-02-14 15:39:06 +0300

    fourth save

---
+++ assignment2.py
@@ -0,0 +1,32 @@
+#Python program
+#Kilometers to miles
+import calendar
+
+# py program display a multiplication table
+
+#display calender
+
+
+
+#1
+num = int(input("Enter kilometer: "))
+print(num*1.60934,"miles")
+
+#multiplication
+number = int(input("Enter number: "))
+print(number,"*1 =",number*1)
+print(number,"*2 =",number*2)
+print(number,"*3 =",number*3)
+print(number,"*4 =",number*4)
+print(number,"*5 =",number*5)
+print(number,"*6 =",number*6)
+print(number,"*7 =",number*7)
+print(number,"*8 =",number*8)
+print(number,"*9 =",number*9)
+
+
+
+#year
+
+
+
